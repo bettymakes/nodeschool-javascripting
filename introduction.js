3daac5ee3264d4999d6a27c2f4d3b3a480c80805
blibotics <blibotics@gmail.com>
2015-04-11 19:34:38 -0400

    Formatting Header

--- introduction.js
+++ introduction.js
@@ -1,1 +1,14 @@
+/* ========================================================================
+
+NODESCHOOL
+Javascripting::Introduction
+
+Instructions:
+Create a file named introduction.js:
+touch introduction.js or if you're on windows, type NUL > introduction.js (type is part of the command!)
+Open the file in your favorite editor, and add this text:
+console.log('hello');
+
+======================================================================== */
+
 console.log("hello");
