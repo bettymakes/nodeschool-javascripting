a85eb10bf151202ac8f0737558dcb09ecc1d0884
blibotics <blibotics@gmail.com>
2015-04-11 23:33:45 -0400

    Part 13 - Accessing Array Values

---
+++ accessing-array-values.js
@@ -0,0 +1,16 @@
+/* ========================================================================
+
+NODESCHOOL
+Javascripting::Array Filtering
+
+Instructions:
+Create a file named accessing-array-values.js.
+In that file, define array food :
+var food = ['apple', 'pizza', 'pear'];
+Use console.log() to print the second value of array to the terminal.
+
+======================================================================== */
+
+var food = ['apple', 'pizza', 'pear'];
+console.log(food[1]);
+
